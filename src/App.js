0a7ff25930a544625f090834184dd6d627e3a931
carlos <chlouzada@gmail.com>
2021-08-21 11:07:38 -0300

    responsividade e custom time

--- src/App.js
+++ src/App.js
@@ -4,24 +4,39 @@ import moment from "moment";
 
 function App() {
   const [date, setDate] = useState("");
+  const [customDate, setCustomDate] = useState("");
   const [minutos, setMinutos] = useState("");
+  const [customTime, setCustomTime] = useState("");
 
   useEffect(() => {
     setDate(moment().add(minutos, "minutes").format("HH:mm"));
-  }, [minutos]);
+    const aux = moment(customTime,"HH:mm").add(minutos, "minutes").format("HH:mm");
+    setCustomDate(aux === 'Invalid date' ? "--:--" : aux);
+    console.log(customTime)
+  }, [minutos,customTime]);
 
   return (
     <div className="App">
       <div className="wrapper">
-        <div className="hora">{date}</div>
+        <div className="hora-custom">{customDate}</div>
         <div class="group">
+          <input
+          className="time"
+            type="time"
+            required
+            onChange={(event) => setCustomTime(event.target.value)}
+            value={customTime}
+          />
+          <span className="bar"></span>
+        </div>
+        <div className="group">
           <input
             type="text"
             required
             onChange={(event) => setMinutos(event.target.value)}
             value={minutos}
           />
-          <span class="bar"></span>
+          <span className="bar"></span>
           <label>Minutos</label>
         </div>
       </div>
